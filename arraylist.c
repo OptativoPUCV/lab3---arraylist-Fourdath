bad1f1eb3ab4eb7d3b91c5d4e4afb722dd0ec69b
Replit user <>
2023-03-31 05:29:29 +0000

    new try

--- arraylist.c
+++ arraylist.c
@@ -48,10 +48,9 @@ void push(ArrayList * l, void * data, int i){
   if (i >= l->size) {
       return; 
   } 
-  l->capacity *= 2;
   if (l->size == l->capacity) {
     void **Dato = malloc(l->capacity * sizeof(void*));
-    
+    l->capacity *= 2;
     for (int k = 0; k < l->size; k++) {
             Dato[k] = l->data[k]; 
     }  
